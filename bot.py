b8b3750b8dea64885b219672d5b62afb1b365e86
sichiiii <76742792+sichiiii@users.noreply.github.com>
2021-07-06 02:57:16 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import telebot, re
+import telebot, re, os
 from sql import SQLITE
 from insert_excel import Insert
 
@@ -10,12 +10,13 @@ insert = Insert()
 def get_text_messages(message):
     mes = message.text
     try:
-        result = re.findall(r'^\d+[\s]\w+[\s]\d\d:\d\d+', mes)
+        result = re.findall(r'^\d+[\s]\S+[\s]\d\d:\d\d+', mes)
         result = re.split(r'[\s]', result[0])
         
         id = result[0]
         name = result[1]
         date = result[2]
+
 
         time = re.split(r'[:]', result[2])
 
@@ -27,7 +28,7 @@ def get_text_messages(message):
         
 
     try:
-        result = re.findall(r'^\w+[\s]\w+[\s]\d\d\*\d\d+[\s]\d+', mes)
+        result = re.findall(r'^\d+[\s]\w+[\s]\S+[\s]\d+', mes)
         result = re.split(r'[\s]', result[0])
         id = result[0]
         name = result[1]
@@ -37,18 +38,47 @@ def get_text_messages(message):
         return result
     except Exception as ex:
         print(str(ex)) 
+
+    try:
+        result = re.findall(r'^\d+[\s]\w+[\s]\S+', mes)
+        result = re.split(r'[\s]', result[0])
+        id = result[0]
+        name = result[1]
+        money = result[2]
+        time = '60'
+        result = sql.addReport(id, name, money, time, message.chat.id)
+        return result
+    except Exception as ex:
+        print(str(ex)) 
     
     if mes == 'F75D20100840C0653AAA5F2BC4240F9C4A299FAB84837C4DABA3AF42ACF081A0':
-        bot.send_message(message.chat.id, 'Enter group ID')
+        bot.send_message(message.chat.id, 'Enter group ID and user ID')
         bot.register_next_step_handler(message, get_records)
+
+    if mes == 'F75D20100840C0653AAA5F2BC4':
+        bot.send_message(message.chat.id, 'Enter group ID')
+        bot.register_next_step_handler(message, get_start_records)
 
 def get_records(message):
     try:
-        chat_id = int(message.text)
-        insert.insert_excel(chat_id)
+        result = re.split(r'[\s]', message.text)
+        insert.insert_excel(int(result[0]), int(result[1]))
         f = open("records.xlsx","rb")
         bot.send_document(message.chat.id, f)
         f = open("reports.xlsx","rb")
+        bot.send_document(message.chat.id, f)
+        os.remove('records.xlsx')
+        os.remove('reports.xlsx')
+        return {'status':'ok'}
+    except Exception as ex:
+        print(str(ex))
+        bot.send_message(message.chat.id, 'Wrong group ID')
+
+def get_start_records(message):
+    try:
+        chat_id = int(message.text)
+        insert.get_start_records(chat_id)
+        f = open("start_records.xlsx","rb")
         bot.send_document(message.chat.id, f)
         return {'status':'ok'}
     except Exception as ex:
--- insert_excel.py
+++ insert_excel.py
@@ -4,13 +4,13 @@ from threading import ExceptHookArgs
 from xlsxwriter.workbook import Workbook
 
 class Insert():
-    def insert_excel(self, chat_id):
+    def insert_excel(self, chat_id, user_id):
         try:
             records = Workbook('records.xlsx')
             worksheet = records.add_worksheet()
             conn=sqlite3.connect('test.db')
             c=conn.cursor()
-            mysel=c.execute(f"select * from records where chat_id='{chat_id}'")
+            mysel=c.execute(f"select * from records where chat_id='{chat_id}' and id='{user_id}'")
 
             for i, row in enumerate(mysel):
                 for j, value in enumerate(row):
@@ -21,12 +21,28 @@ class Insert():
             worksheet = reports.add_worksheet()
             conn=sqlite3.connect('test.db')
             c=conn.cursor()
-            mysel=c.execute(f"select * from reports where chat_id='{chat_id}'")
+            mysel=c.execute(f"select * from reports where chat_id='{chat_id}' and id ='{user_id}'")
 
             for i, row in enumerate(mysel):
                 for j, value in enumerate(row):
                     worksheet.write(i, j, value)
             reports.close()
+            return {'status':'ok'}
+        except Exception as ex:
+            print(str(ex))
+
+    def get_start_records(self, chat_id):
+        try:
+            records = Workbook('start_records.xlsx')
+            worksheet = records.add_worksheet()
+            conn=sqlite3.connect('test.db')
+            c=conn.cursor()
+            mysel=c.execute(f"select * from records where chat_id='{chat_id}'")
+
+            for i, row in enumerate(mysel):
+                for j, value in enumerate(row):
+                    worksheet.write(i, j, value)
+            records.close()
             return {'status':'ok'}
         except Exception as ex:
             print(str(ex))
